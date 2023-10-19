eab9cd39ddaaed6a1075da197072e1926fb232e5
yuki1229h10 <yuki1229.h10@i.softbank.jp>
2023-10-19 10:25:57 +0900

    done

--- 030_スコープ/040_スコープチェーン/start/main.js
+++ 030_スコープ/040_スコープチェーン/start/main.js
@@ -1,18 +1,12 @@
 let a = 2;
 window.a = 4;
+
 function fn1() {
     // let a = 1;
     function fn2() {
+        // let a = 3;
         console.log(a);
-
-        if (true) {
-            var a = 3;
-        }
-
     }
     fn2();
 }
 fn1();
-
-
-
