7677bd53408e6cf073d3640a522d3b65d5c63267
yuki1229h10 <yuki1229.h10@i.softbank.jp>
2023-10-19 10:07:42 +0900

    done

---
+++ 030_スコープ/010_グローバルスコープとスクリプトスコープ/start/main.js
@@ -0,0 +1,11 @@
+let a = 0;
+var b = 0;
+function c() { }
+
+console.log(b);
+
+window.d = 1;
+let d = 2;
+console.log(d);
+
+// window = global
---
+++ 030_スコープ/020_関数スコープとブロックスコープ/start/main.js
@@ -0,0 +1,18 @@
+function a() {
+  let b = 0;
+  console.log(b);
+}
+
+a();
+
+if (true) {
+  var c = 1;
+
+  const d = function () {
+    console.log('d is called');
+  }
+
+}
+
+d();
+console.log(c);
