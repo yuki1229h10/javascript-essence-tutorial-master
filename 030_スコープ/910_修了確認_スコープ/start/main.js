adc119b73aa0cac17f6fc99103baf976244cfc7f
yuki1229h10 <yuki1229.h10@i.softbank.jp>
2023-10-19 20:34:52 +0900

    done

--- 030_スコープ/910_修了確認_スコープ/start/main.js
+++ 030_スコープ/910_修了確認_スコープ/start/main.js
@@ -3,18 +3,34 @@
  * 以下のコードではエラーが発生します。
  * コンソールで"fn called"と表示されるように
  * fn内のコードを変更してください。
- * 
+ *
  * ※if文は削除してはいけません。
  */
 function fn() {
-    if(true) {
-        let a = 'fn called';
+    if (true) {
+        var a = 'fn called';
     }
     return a; // ReferenceError: a is not defined
 }
 
 const result = fn();
 console.log(result);
+
+
+/**
+ * answer
+ */
+function fnAnswer() {
+    let a;
+    if (true) {
+        a = 'fn called';
+    }
+    return a;
+}
+
+const resultAnswer = fnAnswer();
+console.log(resultAnswer);
+
 
 /**
  * 問題２：
@@ -25,8 +41,8 @@ var val = 'val1';
 function fn2() {
     console.log(val); // 期待値->'val1'
 
-    if(true) {
-        var val = 'val2';
+    if (true) {
+        let val = 'val2';
         console.log(val); // 期待値->'val2'
     }
 
@@ -34,15 +50,48 @@ function fn2() {
 }
 fn2();
 
+
 /**
  * 問題３：
  * 「クロージャー（プライベート変数）」のレクチャーで作成
  * したincrementと同じ機能を持つincrement関数をブロック
  * スコープとクロージャーを利用して作成してみてください。
- * 
+ *
  * increment(); // 期待値->1
  * increment(); // 期待値->2
  * increment(); // 期待値->3
  * increment(); // 期待値->4
  */
 
+
+function increment(a) {
+    let num = 0;
+    function increment(b) {
+
+    }
+
+    return {
+        num
+    }();
+}
+
+increment();
+increment();
+increment();
+increment();
+
+
+/**
+ * answer
+ */
+{
+    let num = 0;
+    function increment() {
+        num = num + 1;
+        console.log(num);
+    }
+}
+increment();
+increment();
+increment();
+increment();
--- 030_スコープ/930_修了確認_即時関数/start/main.js
+++ 030_スコープ/930_修了確認_即時関数/start/main.js
@@ -2,32 +2,31 @@
  * 問題：
  * クロージャーの問題で作成した以下のcalcFactoryを即時関数
  * で書き直してみてください。
- */
-function calcFactory(val) {
+*/
+const calc = (function (val) {
     return {
-        plus: function(target) {
+        plus: function (target) {
             const newVal = val + target;
             console.log(`${val} + ${target} = ${newVal}`);
             val = newVal;
         },
-        minus: function(target) {
+        minus: function (target) {
             const newVal = val - target;
             console.log(`${val} - ${target} = ${newVal}`);
             val = newVal;
         },
-        multiply: function(target) {
+        multiply: function (target) {
             const newVal = val * target;
             console.log(`${val} x ${target} = ${newVal}`);
             val = newVal;
         },
-        divide: function(target) {
+        divide: function (target) {
             const newVal = val / target;
             console.log(`${val} / ${target} = ${newVal}`);
             val = newVal;
         },
     }
-}
-const calc = calcFactory(10);
+})(10);
 calc.plus(5);
 calc.minus(3);
 calc.multiply(3);
