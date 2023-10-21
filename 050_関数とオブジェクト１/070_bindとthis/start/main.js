4bf7dff244da51c6e60aac76583d35128d59dd3d
yuki1229h10 <yuki1229.h10@i.softbank.jp>
2023-10-21 12:41:39 +0900

    done

--- 050_関数とオブジェクト１/070_bindとthis/start/main.js
+++ 050_関数とオブジェクト１/070_bindとthis/start/main.js
@@ -2,14 +2,34 @@ window.name = 'John';
 
 const person = {
     name: 'Tom',
-    hello: function() {
+    hello: function () {
         console.log('Hello ' + this.name);
     }
 }
 person.hello();
+
+const helloTom = person.hello.bind(person);
 
 function fn(ref) {
     ref();
 }
 
 fn(person.hello);
+
+fn(helloTom);
+
+
+function a() {
+    console.log('hello ' + this.name);
+}
+
+const b = a.bind({ name: 'Tim' });
+b();
+
+
+function c(name) {
+    console.log('hello ' + name);
+}
+
+const d = c.bind(null, 'Tim');
+d('Joseph');
