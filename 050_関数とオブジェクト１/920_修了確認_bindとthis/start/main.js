719c0b6249158c431804d44f72dd43a623424a11
yuki1229h10 <yuki1229.h10@i.softbank.jp>
2023-10-21 23:26:52 +0900

    done

--- 050_関数とオブジェクト１/920_修了確認_bindとthis/start/main.js
+++ 050_関数とオブジェクト１/920_修了確認_bindとthis/start/main.js
@@ -1,31 +1,43 @@
 const person = {
     name: 'Tom',
-    bye: () => {
+    bye() {
         console.log('Bye ' + this.name);
     },
     hello: function (greeting) {
         console.log(greeting + ' ' + this.name);
         return greeting + ' ' + this.name;
     },
-    /**
-     * 問題４：
-     * 1秒後に"hello Tom"
-     * と出力されるような、メソッドを
-     * personオブジェクトに追加してみてください。
-     * 
-     * 以下のように使用するものとします。
-     * `person.hello1s()` 
-     * -> 1秒後に"hello Tom"と出力
-     * 
-     * 3通りの方法で実装してみてください。
-     * １．bind
-     * ２．アロー関数
-     * ３．thisを一旦変数に代入
-     */
+    hello1s() {
+        // setTimeout(this.hello.bind(this, 'hello'));
 
+        // setTimeout(() => {
+        //     this.hello('hello');
+        // }, 1000);
 
-    
+        const _this = this;
+        setTimeout(function () {
+            _this.hello('hello');
+        });
+    }
 }
+person.hello1s();
+
+/**
+ * 問題４：
+ * 1秒後に"hello Tom"
+ * と出力されるような、メソッドを
+ * personオブジェクトに追加してみてください。
+ *
+ * 以下のように使用するものとします。
+ * `person.hello1s()`
+ * -> 1秒後に"hello Tom"と出力
+ *
+ * 3通りの方法で実装してみてください。
+ * １．bind
+ * ２．アロー関数
+ * ３．thisを一旦変数に代入
+ */
+
 
 /**
  * 問題１：
@@ -33,7 +45,14 @@ const person = {
  * と出力されるように、以下のコード
  * の記載を変更しましょう。
  */
-setTimeout(person.hello, 1000);
+// setTimeout(person.hello.bind(null, 'hello Tom'), 1000);
+
+/**
+ * correct answer
+ */
+setTimeout(person.hello.bind(person, "hello"), 1000);
+
+
 
 /**
  * 問題２：
@@ -41,7 +60,7 @@ setTimeout(person.hello, 1000);
  * と出力されるように、
  * 以下のコードを変更してください。
  */
-alert(person.hello);
+// alert(person.hello('hello'));
 
 /**
  * 問題３：
