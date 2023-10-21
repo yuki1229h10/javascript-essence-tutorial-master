f345905113648542f361e6dc93124af9360b1407
yuki1229h10 <yuki1229.h10@i.softbank.jp>
2023-10-21 22:53:07 +0900

    done

--- 050_関数とオブジェクト１/100_アロー関数とthis/start/main.js
+++ 050_関数とオブジェクト１/100_アロー関数とthis/start/main.js
@@ -1,9 +1,19 @@
 window.name = 'John';
 
+const a = () => console.log('Bye ' + this.name);
+
 const person = {
     name: 'Tom',
-    hello: function() {
+    hello() {
         console.log('Hello ' + this.name);
+        a();
     }
 }
 person.hello();
+
+function b() {
+    const a = () => console.log('Bye ' + this.name);
+    a();
+}
+
+b();
--- 050_関数とオブジェクト１/910_修了確認_コールバック関数/start/main.js
+++ 050_関数とオブジェクト１/910_修了確認_コールバック関数/start/main.js
@@ -3,28 +3,59 @@
  * setTimeoutの実行から１秒後にブラウザの
  * コンソールに'hello Tom'と表示されるように
  * 実装してみましょう。
- * 
+ *
  * ※必ずperson.helloメソッドは解答内で使用してください。
  */
 const person = {
     hello: function () {
+        console.log('hello Tom');
+    }
+}
+setTimeout(person.hello, 1000);
+
+/**
+ * correct answer
+ */
+const personAnswer = {
+    hello: function () {
         return 'hello Tom';
     }
 }
+setTimeout(() => {
+    const hello = personAnswer.hello();
+    console.log(hello);
+}, 1000);
 
-// setTimeout(/** ここに追記 */, 1000);
 
 /**
  * 問題２：
  * setTimeoutの実行から１秒後にブラウザの
  * ダイアログに'hello Tom'と表示されるように
  * 実装してみましょう。
- * 
+ *
  * ※必ずperson.helloメソッドは解答内で使用してください。
  * ※alertは第一引数に渡した文字列を画面のダイアログに表
  * 示する関数です。
  */
+const person2 = {
+    hello: function () {
+        alert('hello Tom');
+    }
+}
+setTimeout(person2.hello, 1000);
 
+/**
+ * correct answer
+ */
+const personAnswer2 = {
+    hello: function () {
+        return 'hello Tom';
+    }
+}
+setTimeout(() => {
+    const hello = personAnswer2.hello();
+    alert(hello);
+}, 1000);
 
 /**
  * 問題３：
@@ -37,7 +68,7 @@ const person = {
  * 'hello'または'hey'のどちらでしょうか？
  */
 const obj = {};
-obj.greeting = function() {
+obj.greeting = function () {
     console.log('hello');
 }
 
@@ -49,10 +80,25 @@ function after1s(callack) {
 // after1s(obj.greeting);
 
 // この後でgreetingを書き換えます。
-obj.greeting = function() {
+obj.greeting = function () {
     console.log('hey');
 }
 
+// helloがheyよりも先に宣言されているため、helloが1秒後に呼び出される
+
+
+/**
+ * correct answer
+ */
+// 別物の関数がセットされているため
+
+let obj1 = {
+    prop: 10
+}
+
+let obj2 = obj1; // 同じ参照
+obj1 = {};
+console.log(obj2);
 
 /**
  * 問題４：
@@ -60,27 +106,27 @@ obj.greeting = function() {
  * コンソール(console.log)に表示するか、
  * ダイアログ(alert)に出力するかを
  * 使い分けできるようにしてください。
- * 
+ *
  * ※コールバック関数を用いて実装してください。
  */
-function calcFactory(val) {
+function calcFactory(val, showAlert) {
     return {
-        plus: function(target) {
+        plus: function (target) {
             const newVal = val + target;
             console.log(`${val} + ${target} = ${newVal}`);
             val = newVal;
         },
-        minus: function(target) {
+        minus: function (target) {
             const newVal = val - target;
             console.log(`${val} - ${target} = ${newVal}`);
             val = newVal;
         },
-        multiply: function(target) {
+        multiply: function (target) {
             const newVal = val * target;
             console.log(`${val} x ${target} = ${newVal}`);
             val = newVal;
         },
-        divide: function(target) {
+        divide: function (target) {
             const newVal = val / target;
             console.log(`${val} / ${target} = ${newVal}`);
             val = newVal;
@@ -88,9 +134,59 @@ function calcFactory(val) {
     };
 }
 
-const calc = calcFactory(10);
-calc.plus(5); 
-calc.minus(3); 
+
+// const calc = calcFactory(10);
+// calc.plus(5, showAlert());
+// calc.minus(3, showAlert());
+// calc.multiply(3, showAlert());
+// calc.divide(2, showAlert());
+
+
+
+
+// function greet(name, callback) {
+//     console.log('Hi ' + ' ' + name);
+//     callback();
+// }
+
+// function callMe() {
+//     console.log('I am callback function');
+// }
+
+// greet('DIO', callMe);
+
+
+/**
+ * correct answer
+ */
+function calcFactory(val, callback) {
+    return {
+        plus: function (target) {
+            const newVal = val + target;
+            callback(`${val} + ${target} = ${newVal}`);
+            val = newVal;
+        },
+        minus: function (target) {
+            const newVal = val - target;
+            callback(`${val} - ${target} = ${newVal}`);
+            val = newVal;
+        },
+        multiply: function (target) {
+            const newVal = val * target;
+            callback(`${val} x ${target} = ${newVal}`);
+            val = newVal;
+        },
+        divide: function (target) {
+            const newVal = val / target;
+            callback(`${val} / ${target} = ${newVal}`);
+            val = newVal;
+        }
+    };
+}
+
+
+const calc = calcFactory(10, alert);
+calc.plus(5);
+calc.minus(3);
 calc.multiply(3);
 calc.divide(2);
-
