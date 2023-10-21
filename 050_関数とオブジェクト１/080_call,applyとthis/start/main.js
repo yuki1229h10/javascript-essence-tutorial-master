f53b9ebae578ab9e236fc67591a247a5e36a89d8
yuki1229h10 <yuki1229.h10@i.softbank.jp>
2023-10-21 19:59:20 +0900

    done

--- 050_関数とオブジェクト１/080_call,applyとthis/start/main.js
+++ 050_関数とオブジェクト１/080_call,applyとthis/start/main.js
@@ -1,7 +1,18 @@
-function a(name) {
-    console.log('hello ' + name);
+function a() {
+    console.log('hello ' + this.name);
 }
 
-const b = a.bind(null, 'Tim');
+const tim = { name: 'Tim' };
+const b = a.bind(tim);
 
 b();
+
+a.apply(tim, ['Tim', 'Bob']);
+a.call(tim, 'Tim', 'Bob');
+
+const arry = [1, 2, 3, 4, 5];
+Math.max(1, 2);
+
+
+const result = Math.max(...arry);
+console.log(result);
