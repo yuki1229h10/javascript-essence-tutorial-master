15996d9d358dd6f1753b79fce2a24786c59c529e
yuki1229h10 <yuki1229.h10@i.softbank.jp>
2023-10-22 17:20:08 +0900

    done

--- 060_関数とオブジェクト２/040_instanceof/start/main.js
+++ 060_関数とオブジェクト２/040_instanceof/start/main.js
@@ -1,7 +1,24 @@
 function F(a, b) {
     this.a = a;
     this.b = b;
-    // return {a: 1};
+    const result = new Object();
+    result.a = 1;
+    return result;
 }
 
-F.prototype.c = function() {}
+F.prototype.c = function () { }
+
+const instance = new F(1, 2);
+console.log(instance instanceof F);
+console.log(instance.__proto__ === F.prototype);
+
+function fn(arg) {
+    if (arg instanceof Array) {
+        arg.push('value');
+    } else {
+        arg['key'] = 'value';
+    }
+    console.log(arg);
+}
+
+fn([]);
