d1cedc68ddbf97936410db2480e1b55212abc93c
yuki1229h10 <yuki1229.h10@i.softbank.jp>
2023-10-22 18:04:38 +0900

    done

--- 060_関数とオブジェクト２/080_プロトタイプ継承/start/main.js
+++ 060_関数とオブジェクト２/080_プロトタイプ継承/start/main.js
@@ -3,6 +3,21 @@ function Person(name, age) {
   this.age = age;
 }
 
-Person.prototype.hello = function() {
+Person.prototype.hello = function () {
   console.log('hello ' + this.name);
 }
+
+function Japanese(name, age) {
+  Person.call(this, name, age);
+}
+
+Japanese.prototype = Object.create(Person.prototype);
+
+Person.prototype.hello = function () {
+  console.log('hello ' + this.name);
+}
+
+const taro = new Japanese('Taro', 23);
+console.log(taro);
+
+taro.hello();
