d3113e34dbea67a9cf42ae5ae11cd7eab054ea50
yuki1229h10 <yuki1229.h10@i.softbank.jp>
2023-10-22 18:19:37 +0900

    done

--- 060_関数とオブジェクト２/100_クラス継承/start/main.js
+++ 060_関数とオブジェクト２/100_クラス継承/start/main.js
@@ -1,25 +1,29 @@
-function Person(name, age) {
-  this.name = name;
-  this.age = age;
+class Person {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+
+  hello() {
+    console.log('hello ' + this.name);
+  }
 }
 
-Person.prototype.hello = function() {
-  console.log('hello ' + this.name);
-}
+class Japanese extends Person {
+  constructor(name, age, gender) {
+    super(name, age);
+    this.gender = gender;
+  }
 
-function Japanese(name, age, gender) {
-  Person.call(this, name, age);
-  this.gender = gender;
-}
+  hello() {
+    console.log('hi ' + this.name);
+  }
 
-Japanese.prototype = Object.create(Person.prototype);
-
-Japanese.prototype.hello = function() {
-  console.log('Konnichiwa ' + this.name);
-}
-
-Japanese.prototype.bye = function() {
-  console.log('Sayonara ' + this.name);
+  bye() {
+    console.log('bye ' + this.name);
+  }
 }
 
 const taro = new Japanese('Taro', 23, 'Male');
+console.log(taro);
+taro.bye();
