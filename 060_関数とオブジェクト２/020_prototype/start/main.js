c3743cca0afeee0586c87f858c5e116d23597ee6
yuki1229h10 <yuki1229.h10@i.softbank.jp>
2023-10-22 12:55:41 +0900

    done

--- 060_関数とオブジェクト２/020_prototype/start/main.js
+++ 060_関数とオブジェクト２/020_prototype/start/main.js
@@ -3,6 +3,14 @@ function Person(name, age) {
     this.age = age;
 }
 
+// 余分なメモリを食わない
+Person.prototype.hello = function () {
+    console.log('hello ' + this.name);
+}
+
 const bob = new Person('Bob', 18);
 const tom = new Person('Tom', 33);
 const sun = new Person('Sun', 20);
+
+bob.hello();
+tom.hello();
