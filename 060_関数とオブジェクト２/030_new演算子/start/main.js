8eb04549bf381106b20ff7be656dac35503ce5a8
yuki1229h10 <yuki1229.h10@i.softbank.jp>
2023-10-22 13:15:06 +0900

    done

---
+++ 060_関数とオブジェクト２/030_new演算子/start/main.js
@@ -0,0 +1,18 @@
+function F(a, b) {
+  this.a = a;
+  this.b = b;
+  return { a: 1 };
+}
+
+F.prototype.c = function () { }
+
+function newOpe(C, ...args) {
+  const _this = Object.create(C.prototype);
+  const result = C.apply(_this, args);
+  console.log(typeof result);
+  if (typeof result === "object" && result !== null)
+    return result;
+}
+
+const instance = new newOpe(F, 1, 2);
+console.log(instance);
