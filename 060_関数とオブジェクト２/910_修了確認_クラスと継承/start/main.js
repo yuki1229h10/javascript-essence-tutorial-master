31b148ce5c5c59c84056619b78236be6594dfce0
yuki1229h10 <yuki1229.h10@i.softbank.jp>
2023-10-23 16:42:14 +0900

    done

--- 060_関数とオブジェクト２/910_修了確認_クラスと継承/start/main.js
+++ 060_関数とオブジェクト２/910_修了確認_クラスと継承/start/main.js
@@ -3,41 +3,41 @@
  * ログインの制御を行うloginController
  * という名前の関数が存在します。
  * この関数はuserインスタンスを渡すと
- * 
+ *
  * ログイン処理（login）
  * ->ユーザー権限チェック（checkRoll）
  * ->適切なページへのリダイレクト（redirect）
- * 
+ *
  * を内部で行います。
- * 
+ *
  * 以下の呼び出し方をした場合に該当のメッセージが
  * コンソールに出るように実装してみてください。
- * 
- **************************************** 
+ *
+ ****************************************
  * １．一般ユーザーがログインした時。
  * loginController(new User('Bob'));
- * 
+ *
  * ログイン成功した場合：
  * User: Bob
  * you have normal roll
  * redirect : /
  * login success
- * 
+ *
  * ログイン失敗した場合：
  * User: Bob
  * you have normal roll
  * login failed <- checkRollで失敗した場合
- * 
+ *
  ****************************************
  * ２．管理者（AdminUser）でログインした場合
  * loginController(new AdminUser('Bob'));
- * 
+ *
  * ログイン成功した場合：
  * User: Bob
  * you have admin roll
  * redirect : /admin
  * login success
- * 
+ *
  * ログイン失敗した場合：
  * User: Bob
  * login failed <- loginで失敗した場合
@@ -52,3 +52,61 @@ function loginController(user) {
     console.log('login failed');
   }
 }
+
+
+function loginController(user) {
+  if (user.login()) {
+    if (user.checkRole()) {
+      user.redirect();
+      console.log('login success');
+    } else {
+      console.log('User: ' + user.name);
+      console.log('you have normal roll');
+      console.log('login failed');
+    }
+  } else {
+    console.log('User: ' + user.name);
+    console.log('login failed');
+  }
+}
+
+
+/**
+ * correct answer
+ */
+class User {
+  constructor(name) {
+    this.name = name;
+    this.redirectTo = '/';
+  }
+
+  login() {
+    console.log(`User: ${this.name}`);
+    return true;
+  }
+
+  checkRoll() {
+    console.log('you have normal roll');
+    return true;
+  }
+
+  redirect() {
+    console.log(`redirect : ${this.redirectTo}`);
+    return true;
+  }
+}
+
+loginController(new User('Bob'));
+
+class AdminUser extends User {
+  constructor(name) {
+    super(name);
+  }
+
+  checkRoll() {
+    console.log('you have admin roll');
+    return true;
+  }
+}
+
+// classのimageをつかむ
