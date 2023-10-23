b4b7090b5b3f26e666e538e78f9e2a749e12c3fc
yuki1229h10 <yuki1229.h10@i.softbank.jp>
2023-10-23 17:08:54 +0900

    done

--- 060_関数とオブジェクト２/930_修了確認_チェーンメソッド/start/main.js
+++ 060_関数とオブジェクト２/930_修了確認_チェーンメソッド/start/main.js
@@ -2,21 +2,21 @@
  * 問題：
  * 電卓の入力と同じような挙動をするチェーンメソッド
  * を作成してみましょう。
- * 
+ *
  * 例えば、次のように使用し、結果が表示
  * されるようにします。
- * 
- * 
+ *
+ *
  * 例１）
  * const calc = new Calculator();
- * 
+ *
  * calc.set(10)
  * .minus()
  * .set(3) -> '7'を出力(10 - 3)
- * 
+ *
  * 例２）
  * const calc = new Calculator();
- * 
+ *
  * calc.set(10)
  * 	.minus()
  * 	.set(3) -> '7'を出力
@@ -24,14 +24,111 @@
  * 	.set(6) -> '42'を出力（10 - 3) * 6
  */
 
-const calc = new Calculator();
+// const calc = new Calculator();
 
-calc.set(10)
-	.minus()
-	.set(3)
-	.mutiply()
-	.set(6)
-	.divide()
-	.set(2)
-	.plus()
-	.set(2)
+// calc.set(10)
+// 	.minus()
+// 	.set(3)
+// 	.mutiply()
+// 	.set(6)
+// 	.divide()
+// 	.set(2)
+// 	.plus()
+// 	.set(2)
+
+
+// class Calculator {
+// 	constructor() {
+// 		this.result = 0;
+// 	}
+
+// 	set(value) {
+// 		this.result = value;
+// 		return this;
+// 	}
+
+// 	plus() {
+// 		return this;
+// 	}
+
+// 	minus() {
+// 		return this;
+// 	}
+
+// 	multiply() {
+// 		return this;
+// 	}
+
+// 	divide() {
+// 		return this;
+// 	}
+// }
+
+// const calc = new Calculator();
+// const finalResult = calc
+// 	.set(10)
+// 	.minus(1);
+
+// console.log(finalResult);
+
+
+/**
+ * correct answer
+ */
+class Calculator {
+	constructor() {
+		this.val = null;
+		this._operator;
+	}
+
+	set(val) {
+
+		if (this.val === null) {
+			this.val = val;
+		} else {
+			this._operator(this.val, val);
+		}
+		return this;
+	}
+
+	plus() {
+		this._operator = function (val1, val2) {
+			const result = val1 + val2;
+			this._equal(result);
+		}
+		return this;
+	}
+
+	minus() {
+		this._operator = function (val1, val2) {
+			const result = val1 - val2;
+			this._equal(result);
+		}
+		return this;
+	}
+
+	multiply() {
+		this._operator = function (val1, val2) {
+			const result = val1 * val2;
+			this._equal(result);
+		}
+		return this;
+	}
+
+	divide() {
+		this._operator = function (val1, val2) {
+			const result = val1 / val2;
+			this._equal(result);
+		}
+		return this;
+	}
+
+	_equal(result) {
+		this.val = result;
+		console.log(result);
+	}
+
+}
+
+const calc = new Calculator();
+calc.set(10).plus().set(3)
