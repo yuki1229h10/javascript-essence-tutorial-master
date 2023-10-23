34b88a174b5fc4876a717682223c767c68149bbf
yuki1229h10 <yuki1229.h10@i.softbank.jp>
2023-10-23 16:52:32 +0900

    done

--- 060_関数とオブジェクト２/920_修了確認_クラスとコールバック/start/main.js
+++ 060_関数とオブジェクト２/920_修了確認_クラスとコールバック/start/main.js
@@ -2,22 +2,37 @@
  * 問題：
  * Person.helloメソッドをsetTimeoutで１秒後に
  * 実行しようとしましたが、"hello Bob"と表示されませんでした。
- * 
+ *
  * setTimeoutに渡す方法がおかしそうなのですが、
  * どのようにすればよいでしょうか？
- * 
+ *
  * ※２通りの方法で実装してみてください。
  */
 class Person {
   constructor(name, age) {
-      this.name = name;
-      this.age = age;
+    this.name = name;
+    this.age = age;
   }
 
   hello() {
-      console.log('hello ' + this.name);
+    console.log('hello ' + this.name);
   }
 }
 
 const bob = new Person('Bob', 23);
-setTimeout(bob.hello, 1000);
+// setTimeout(bob.hello, 1000);
+
+
+setTimeout(() => {
+  bob.hello();
+}, 1000);
+
+setTimeout(bob.hello.bind(bob), 1000);
+
+
+/**
+ * correct answer
+ */
+setTimeout(function () {
+  bob.hello();
+}, 1000);
