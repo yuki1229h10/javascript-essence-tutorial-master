1944dec72e847be105077196a2360e3fd994501b
yuki1229h10 <yuki1229.h10@i.softbank.jp>
2023-10-24 18:38:42 +0900

    done

--- 070_反復処理/040_forinと列挙可能性/start/main.js
+++ 070_反復処理/040_forinと列挙可能性/start/main.js
@@ -1,5 +1,23 @@
+const s = Symbol();
+
 const obj = {
 	prop1: 'value1',
 	prop2: 'value2',
-	prop3: 'value3'
+	prop3: 'value3',
+	[s]: 'value4'
+}
+
+Object.prototype.method = function () { }
+Object.defineProperty(Object.prototype, 'method', {
+	enumerable: false
+});
+
+const d = Object.getOwnPropertyDescriptor(Object.prototype, 'method');
+const e = Object.getOwnPropertyDescriptor(obj.s);
+console.log(d);
+
+for (let key in obj) {
+	if (obj.hasOwnProperty(key)) {
+		console.log(key, obj[key]);
+	}
 }
