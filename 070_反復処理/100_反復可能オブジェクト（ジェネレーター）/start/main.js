0f4bd4f5c6003ebbcc50bc15b1134d9e204f1ace
yuki1229h10 <yuki1229.h10@i.softbank.jp>
2023-10-25 16:47:14 +0900

    done

--- 070_反復処理/100_反復可能オブジェクト（ジェネレーター）/start/main.js
+++ 070_反復処理/100_反復可能オブジェクト（ジェネレーター）/start/main.js
@@ -3,3 +3,13 @@ const items = {
 	prop2: 'value2',
 	prop3: 'value3'
 }
+
+Object.prototype[Symbol.iterator] = function* () {
+	for (let key in this) {
+		yield [key, this[key]];
+	}
+}
+
+for (let [k, v] of items) {
+	console.log(k, v);
+}
