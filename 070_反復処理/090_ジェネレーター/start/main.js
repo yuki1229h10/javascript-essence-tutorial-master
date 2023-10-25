d9d250e154dccb377b5ebc35c2a885dd16b55b08
yuki1229h10 <yuki1229.h10@i.softbank.jp>
2023-10-25 16:42:07 +0900

    done

---
+++ 070_反復処理/090_ジェネレーター/start/main.js
@@ -0,0 +1,43 @@
+// function* gen() {
+//   yield 1;
+//   yield 2;
+//   return 3;
+// }
+
+// const it = gen();
+// console.log(it.next());
+// console.log(it.next());
+// console.log(it.next());
+// console.log(it.next());
+
+
+function genIterator(max = 10) {
+  let i = 0;
+
+  return {
+    next: function () {
+      if (i >= max) {
+        return {
+          done: true
+        }
+      } else {
+        return {
+          done: false,
+          value: i++
+        }
+      }
+    }
+  }
+}
+
+const it = genIterator();
+
+
+function* genIterator2(max = 10) {
+  let i = 0;
+
+  while (i < max) {
+    yield i++;
+  }
+  return;
+}
